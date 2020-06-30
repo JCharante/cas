a87b42119496a6ee36806a1283379c2c048bef0d
JCharante <git@jcharante.com>
2020-06-30 15:17:24 +0700

    add compilation settings so that we can use this package on the api server, not just web

---
+++ types/index.ts
@@ -0,0 +1,49 @@
+export type Email = string;
+
+export type SessionKey = string;
+
+export interface RedesignedUser {
+    email: Email;
+    isAdmin: boolean;
+}
+
+export interface UserWithSessionKey extends RedesignedUser {
+    sessionKey: SessionKey;
+}
+
+export interface AuthLoginResponse {
+    message: string;
+    data: UserWithSessionKey;
+}
+
+export interface AuthSignupResponse extends AuthLoginResponse {}
+
+export enum ErrorTypes {
+    InvalidSessionKey,
+    UserIsMissing
+}
+
+export interface CasError {
+    type: ErrorTypes;
+    debugMessage: string;
+    displayableMessage: string;
+}
+
+export interface AuthAccountInfoResponse {
+    data?: {
+        email: string;
+        isAdmin: boolean;
+    }
+
+    hasError: boolean;
+
+    error?: CasError;
+}
+
+export function isCasError(object: any): object is CasError {
+    return 'displayableMessage' in object;
+}
+
+export function returnTrue(): boolean {
+    return true;
+}
