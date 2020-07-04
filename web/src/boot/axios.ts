15e6aaf9adf265fb847f1614cd8aedbd007c7845
JCharante <git@jcharante.com>
2020-07-04 17:32:19 -0400

    update gitignore and package lock

--- web/src/boot/axios.ts
+++ web/src/boot/axios.ts
@@ -2,7 +2,8 @@ import axios, { AxiosInstance } from 'axios';
 import { boot } from 'quasar/wrappers';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:3300',
+  // baseURL: 'http://127.0.0.1:3300',
+  baseURL: 'https://cas-api.jcharante.com',
 });
 
 declare module 'vue/types/vue' {
